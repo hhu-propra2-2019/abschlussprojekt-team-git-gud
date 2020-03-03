9cb65322dc980ea21ae7292a6cf67ae7e9dd78d0
matlorr <matvey.lorkish@outlook.de>
2020-03-03 13:28:15 +0100

    ADD project folder

---
+++ src/main/java/de/hhu/propra2/material2/Material2Application.java
@@ -0,0 +1,13 @@
+package de.hhu.propra2.material2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Material2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Material2Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/hhu/propra2/material2/Material2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.propra2.material2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Material2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

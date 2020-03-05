06325b98eb9598c1ce3fd8e9f3ea2cfbdd506149
Xeadriel <ogulcanasik@web.de>
2020-03-05 14:16:08 +0100

    ADD TagDTO

---
+++ src/main/java/de/hhu/propra2/material2/mops/DTOs/TagDTO.java
@@ -0,0 +1,18 @@
+package de.hhu.propra2.material2.mops.DTOs;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TagDTO {
+        final int id;
+        final String text;
+        final List<DateiDTO> dateien;
+
+    public TagDTO(int id, String text, List<DateiDTO> dateien) {
+        this.id = id;
+        this.text = text;
+        this.dateien = dateien;
+    }
+}

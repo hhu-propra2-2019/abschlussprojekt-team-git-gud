47b985bee5842c6e619525372d486dbf1b5a6064
Xeadriel <ogulcanasik@web.de>
2020-03-05 14:15:49 +0100

    ADD DateiDTO

---
+++ src/main/java/de/hhu/propra2/material2/mops/DTOs/DateiDTO.java
@@ -0,0 +1,34 @@
+package de.hhu.propra2.material2.mops.DTOs;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class DateiDTO {
+    final int id;
+    final String name;
+    final String pfad;
+    final String uploader;
+    final List<TagDTO> tagDTOS;
+    final GruppeDTO gruppe;
+    final Date uploaddatum;
+    final Date veroeffentlichungsdatum;
+    final double dateigroesse;
+    final String dateityp;
+
+
+    public DateiDTO(int id, String name, String pfad, String uploader, List<TagDTO> tagDTOS, Date uploaddatum, Date veroeffentlichungsdatum, double dateigroesse, String dateityp, GruppeDTO gruppe) {
+        this.id = id;
+        this.name = name;
+        this.pfad = pfad;
+        this.uploader = uploader;
+        this.tagDTOS = tagDTOS;
+        this.gruppe = gruppe;
+        this.uploaddatum = uploaddatum;
+        this.veroeffentlichungsdatum = veroeffentlichungsdatum;
+        this.dateigroesse = dateigroesse;
+        this.dateityp = dateityp;
+    }
+}

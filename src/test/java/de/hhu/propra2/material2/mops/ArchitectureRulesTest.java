f63627b81b55af775e6fdd7fb886d70273886754
magoo100 <magoo100@uni-duesseldorf.de>
2020-03-05 14:43:33 +0100

    FIX errrors in ArchitectureRulesTest

--- src/test/java/de/hhu/propra2/material2/mops/ArchitectureRulesTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/ArchitectureRulesTest.java
@@ -1,20 +1,20 @@
 package de.hhu.propra2.material2.mops;
 
-import com.sun.tools.javac.Main;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-@AnalyzeClasses(packagesOf = Main.class)
+@AnalyzeClasses(packagesOf = Material2Application.class)
 public class ArchitectureRulesTest {
 
     @ArchTest
     static ArchRule layer_dependencies_are_respected = layeredArchitecture()
             .layer("persistence").definedBy("..persistence..")
             .layer("domain").definedBy("..domain..")
-            .layer("gui").definedBy("..web..")
+            .layer("gui").definedBy("..Controller..")
             .whereLayer("gui").mayNotBeAccessedByAnyLayer()
-            .whereLayer("businesslogic").mayOnlyBeAccessedByLayers("gui");
+            .whereLayer("domain").mayOnlyBeAccessedByLayers("gui")
+            .whereLayer("persistence").mayOnlyBeAccessedByLayers("domain");
 }

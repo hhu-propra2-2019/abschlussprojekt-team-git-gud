dd967cc06ac651a399f46c5ce6b8a71c0d919f25
Koybu <sasch223@hhu.de>
2020-03-06 14:45:01 +0100

    ADJUST ModelService Style

--- src/main/java/de/hhu/propra2/material2/mops/ModelService.java
+++ src/main/java/de/hhu/propra2/material2/mops/ModelService.java
@@ -26,37 +26,38 @@ public class ModelService {
     private final GruppeRepository gruppen;
     private final UserRepository users;
 
-    public ModelService(DateiRepository dateien,GruppeRepository gruppen,UserRepository users){
+    public ModelService(DateiRepository dateien, GruppeRepository gruppen, UserRepository users) {
         this.dateien = dateien;
         this.gruppen = gruppen;
         this.users = users;
     }
 
-    public Datei load(DateiDTO dto){
+    public Datei load(DateiDTO dto) {
         //List<Tag> tags = dto.getTagDTOS().stream().map(this::load).collect(Collectors.toList());
         List<Tag> tags = new ArrayList<>();
         dto.getTagDTOs().forEach(tagDTO -> tags.add(load(tagDTO)));
-        return new Datei(dto.getId(),dto.getName(),dto.getPfad(),load(dto.getUploader()),tags,dto.getUploaddatum(),dto.getVeroeffentlichungsdatum(),dto.getDateigroesse(),dto.getDateityp());
+        return new Datei(dto.getId(), dto.getName(), dto.getPfad(), load(dto.getUploader()), tags, dto.getUploaddatum(), dto.getVeroeffentlichungsdatum(), dto.getDateigroesse(), dto.getDateityp());
     }
 
-    public Tag load(TagDTO dto){
-        return new Tag(dto.getId(),dto.getText());
+    public Tag load(TagDTO dto) {
+        return new Tag(dto.getId(), dto.getText());
     }
 
-    public User load(UserDTO dto){
-        HashMap<Gruppe,Boolean> belegungUndRechte = new HashMap<>();
-        for(GruppeDTO gruppeDTO : dto.getBelegungUndRechte().keySet() ) {
+    public User load(UserDTO dto) {
+        HashMap<Gruppe, Boolean> belegungUndRechte = new HashMap<>();
+        for (GruppeDTO gruppeDTO : dto.getBelegungUndRechte().keySet()) {
             belegungUndRechte.put(load(gruppeDTO), dto.getBelegungUndRechte().get(gruppeDTO));
         }
 
-        return new User(dto.getId(),dto.getVorname(),dto.getNachname(),dto.getKeycloakname(),belegungUndRechte);
-    }
-    public Gruppe load(GruppeDTO dto){
-        List<Datei> zugehoerigeDateien = dto.getDateien().stream().map(this::load).collect(Collectors.toList());
-        return new Gruppe(dto.getId(),dto.getName(),zugehoerigeDateien);
+        return new User(dto.getId(), dto.getVorname(), dto.getNachname(), dto.getKeycloakname(), belegungUndRechte);
     }
 
-    public List<Gruppe> getAlleGruppenByUser(Long id){
+    public Gruppe load(GruppeDTO dto) {
+        List<Datei> zugehoerigeDateien = dto.getDateien().stream().map(this::load).collect(Collectors.toList());
+        return new Gruppe(dto.getId(), dto.getName(), zugehoerigeDateien);
+    }
+
+    public List<Gruppe> getAlleGruppenByUser(Long id) {
         User user = load(users.findById(id).get());
         return user.getAllGruppen();
     }

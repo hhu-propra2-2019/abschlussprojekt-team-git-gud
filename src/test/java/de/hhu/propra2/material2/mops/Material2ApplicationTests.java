b935ebcc1a4a756604bbf1f835db3ca9ed87c64d
magoo100 <magoo100@uni-duesseldorf.de>
2020-03-09 13:49:47 +0100

    fill test method contextLoads

--- src/test/java/de/hhu/propra2/material2/mops/Material2ApplicationTests.java
+++ src/test/java/de/hhu/propra2/material2/mops/Material2ApplicationTests.java
@@ -3,11 +3,14 @@ package de.hhu.propra2.material2.mops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Material2ApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1, 1);
     }
 
 }

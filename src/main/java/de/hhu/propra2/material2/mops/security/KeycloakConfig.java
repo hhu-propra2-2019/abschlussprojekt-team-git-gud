6fab1b71e067a92bd8e0c561055d358482f9e380
Jakhes <dean.schmitz@schmitzbauer.de>
2020-03-10 14:58:44 +0100

    SETUP KeyCloak

---
+++ src/main/java/de/hhu/propra2/material2/mops/security/KeycloakConfig.java
@@ -0,0 +1,22 @@
+package de.hhu.propra2.material2.mops.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * WORKAROUND for https://issues.redhat.com/browse/KEYCLOAK-11282
+ * Bean should move into {@link SecurityConfiguration} if Bug has been resolved
+ */
+@Configuration
+public class KeycloakConfig {
+
+    /**KeycloakConfigResolver.
+     * @return KeycloakSpringBootConfigResolver
+     */
+    @SuppressWarnings("checkstyle:MethodName")
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}

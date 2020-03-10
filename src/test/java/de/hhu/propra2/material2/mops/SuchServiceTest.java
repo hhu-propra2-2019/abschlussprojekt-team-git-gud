7ec62a5078496a7fed928f18c819d12c196a2e76
Koybu <sasch223@hhu.de>
2020-03-10 16:19:33 +0100

    ADD keine_Filter()

--- src/test/java/de/hhu/propra2/material2/mops/SuchServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/SuchServiceTest.java
@@ -9,16 +9,20 @@ import de.hhu.propra2.material2.mops.domain.models.Gruppe;
 import de.hhu.propra2.material2.mops.domain.models.Tag;
 import de.hhu.propra2.material2.mops.domain.models.User;
 import de.hhu.propra2.material2.mops.domain.services.ModelService;
+import de.hhu.propra2.material2.mops.models.Suche;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class SuchServiceTest {
@@ -43,6 +47,10 @@ public class SuchServiceTest {
     private SuchService suchService;
     private List<Datei> dateienGruppe1;
     private List<Datei> dateienGruppe2;
+    private Datei datei1;
+    private Datei datei2;
+    private Datei datei3;
+    private Datei datei4;
 
     @BeforeEach
     public void setUp() {
@@ -51,11 +59,12 @@ public class SuchServiceTest {
                 userRepoMock,
                 modelServiceMock);
 
-        when(modelServiceMock.load(any(UserDTO.class))).thenReturn(userMock);
+        //TODO Mockito.lenient() = eig stub?
+        Mockito.lenient().when(modelServiceMock.load(any(UserDTO.class))).thenReturn(userMock);
 
         //Date for Datei
         Calendar calender = Calendar.getInstance();
-        calender.set(2020, 1,3);
+        calender.set(2020, 1, 3);
         Date date1 = calender.getTime();
         calender.set(2020, 3, 5);
         Date date2 = calender.getTime();
@@ -67,26 +76,21 @@ public class SuchServiceTest {
         Tag tag2 = new Tag(2, "Relevant");
         Tag tag3 = new Tag(3, "Übung");
         Tag tag4 = new Tag(4, "Irgendwas");
-        List<Tag> tags1= new ArrayList<>(Arrays.asList(tag1, tag2, tag3, tag4));
-        List<Tag> tags2= new ArrayList<>(Arrays.asList(tag1));
-        List<Tag> tags3= new ArrayList<>(Arrays.asList(tag2, tag4));
+        List<Tag> tags1 = new ArrayList<>(Arrays.asList(tag1, tag2, tag3, tag4));
+        List<Tag> tags2 = new ArrayList<>(Arrays.asList(tag1));
+        List<Tag> tags3 = new ArrayList<>(Arrays.asList(tag2, tag4));
 
         //Dateien for List<Datei>
-        Datei datei1 = new Datei(1, "1", "a/b/2", uploaderMock, tags1,
+        datei1 = new Datei(1, "1", "a/b/2", uploaderMock, tags1,
                 date1, veroeffentlichung, 1, "pdf");
-        Datei datei2 = new Datei(2, "2", "a/b/2", uploaderMock, tags2,
+        datei2 = new Datei(2, "2", "a/b/2", uploaderMock, tags2,
                 date1, veroeffentlichung, 1, "pdf");
-        Datei datei3 = new Datei(3, "3", "a/b/3", uploaderMock, tags3,
+        datei3 = new Datei(3, "3", "a/b/3", uploaderMock, tags3,
                 date1, veroeffentlichung, 1, "jpg");
-        Datei datei4 = new Datei(4, "4", "a/b/4", uploaderMock, tags3,
+        datei4 = new Datei(4, "4", "a/b/4", uploaderMock, tags3,
                 date2, veroeffentlichung, 1, "jpg");
-        dateienGruppe1.add(datei1);
-        dateienGruppe1.add(datei2);
-        dateienGruppe1.add(datei3);
-        dateienGruppe1.add(datei4);
-        dateienGruppe2.add(datei1);
-        dateienGruppe2.add(datei2);
-        dateienGruppe2.add(datei3);
+        dateienGruppe1 = new ArrayList<>(Arrays.asList(datei1, datei2, datei3, datei4));
+        dateienGruppe2 = new ArrayList<>(Arrays.asList(datei1, datei2, datei3));
 
         when(gruppeMock1.getDateien()).thenReturn(dateienGruppe1);
         when(gruppeMock2.getDateien()).thenReturn(dateienGruppe2);
@@ -94,8 +98,22 @@ public class SuchServiceTest {
     }
 
     @Test
-    public void keine_Suchparameter() {
+    public void keine_Filter() {
+        Suche suche = new Suche(
+                "01.01.2000",
+                "31.12.2100",
+                null,
+                null,
+                null,
+                null,
+                null);
 
+        List<Datei> result = suchService.starteSuche(suche, "Peter");
+
+        assertTrue(result.contains(datei1));
+        assertTrue(result.contains(datei2));
+        assertTrue(result.contains(datei3));
+        assertTrue(result.contains(datei4));
     }
 
 }

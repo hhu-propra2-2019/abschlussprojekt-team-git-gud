cf62723e3651662cfd4354ebd72c08a5705fb9db
D4sherInc <phil.klotzsche@gmail.com>
2020-03-12 13:27:24 +0100

    CREATE setUp with new instance of ModelService with mocked repos

--- src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
@@ -5,11 +5,17 @@ import de.hhu.propra2.material2.mops.Database.GruppeRepository;
 import de.hhu.propra2.material2.mops.Database.UserRepository;
 import de.hhu.propra2.material2.mops.domain.models.Gruppe;
 import de.hhu.propra2.material2.mops.domain.models.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = ModelService.class)
 public class ModelServiceTest {
 
 
@@ -29,4 +35,21 @@ public class ModelServiceTest {
     private Gruppe gruppeMock2;
     @Mock
     private User uploaderMock;
+    private ModelService modelService;
+
+    /**
+     * setUp for every test.
+     * Creates a new ModelService object with mocked repositories.
+     */
+    @BeforeEach
+    public void setUp() {
+        this.modelService = new ModelService(dateiRepoMock,
+                gruppenRepoMock,
+                userRepoMock);
+    }
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
 }

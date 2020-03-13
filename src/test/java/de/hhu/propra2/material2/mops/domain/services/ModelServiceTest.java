2dea025bd2636549cfdcf05df609db51c2f32b2f
D4sherInc <phil.klotzsche@gmail.com>
2020-03-13 17:00:33 +0100

    ADD 2 tests: one null check for a getter and a test to check the convert of GroupHashMaps

--- src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
@@ -1,23 +1,26 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
+import de.hhu.propra2.material2.mops.Database.DTOs.GruppeDTO;
+import de.hhu.propra2.material2.mops.Database.DTOs.UserDTO;
 import de.hhu.propra2.material2.mops.Database.DateiRepository;
 import de.hhu.propra2.material2.mops.Database.GruppeRepository;
 import de.hhu.propra2.material2.mops.Database.UserRepository;
 import de.hhu.propra2.material2.mops.domain.models.Gruppe;
-import de.hhu.propra2.material2.mops.domain.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest(classes = ModelService.class)
 public class ModelServiceTest {
-
 
     @Mock
     private DateiRepository dateiRepoMock;
@@ -26,15 +29,12 @@ public class ModelServiceTest {
     @Mock
     private UserRepository userRepoMock;
     @Mock
-    private ModelService modelServiceMock;
+    private GruppeDTO gruppeDTOMock1;
     @Mock
-    private User userMock;
-    @Mock
-    private Gruppe gruppeMock1;
-    @Mock
-    private Gruppe gruppeMock2;
-    @Mock
-    private User uploaderMock;
+    private GruppeDTO gruppeDTOMock2;
+
+    private UserDTO angelaDTO;
+
     private ModelService modelService;
 
     /**
@@ -42,6 +42,7 @@ public class ModelServiceTest {
      * Creates a new ModelService object with mocked repositories.
      */
     @BeforeEach
+    @SuppressWarnings("checkstyle:magicnumber")
     public void setUp() {
         this.modelService = new ModelService(dateiRepoMock,
                 gruppenRepoMock,
@@ -49,7 +50,28 @@ public class ModelServiceTest {
     }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void getAlleGruppenByUserNameIsNull() {
+        when(userRepoMock.findByKeycloakname("")).thenReturn(null);
+        List<Gruppe> result = modelService.getAlleGruppenByUser("");
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertGroupHashMapTwoGroups() {
+        when(gruppeDTOMock1.getId()).thenReturn(1L);
+        when(gruppeDTOMock2.getId()).thenReturn(2L);
+
+        HashMap<GruppeDTO, Boolean> belegungUndRechte = new HashMap<>();
+        belegungUndRechte.put(gruppeDTOMock1, true);
+        belegungUndRechte.put(gruppeDTOMock2, false);
+        angelaDTO = new UserDTO(1,
+                "Angela",
+                "Merkel",
+                "AngelaKCName",
+                belegungUndRechte);
+
+        HashMap<Gruppe, Boolean> result = modelService.convertHashMapGruppeDTOtoGruppe(angelaDTO);
+
+        assertThat(result.size(), is(2));
     }
 }

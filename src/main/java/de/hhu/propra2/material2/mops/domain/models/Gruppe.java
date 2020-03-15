b085bce9344adaf2de1a6e392870400269971b4c
matlorr <matvey.lorkish@outlook.de>
2020-03-15 17:50:08 +0100

    Fill Gruppe

--- src/main/java/de/hhu/propra2/material2/mops/domain/models/Gruppe.java
+++ src/main/java/de/hhu/propra2/material2/mops/domain/models/Gruppe.java
@@ -1,4 +1,12 @@
 package de.hhu.propra2.material2.mops.domain.models;
 
+import lombok.Value;
+
+import java.util.List;
+
+@Value
 public class Gruppe {
+    private long id;
+    private String name;
+    private List<Datei> dateien;
 }

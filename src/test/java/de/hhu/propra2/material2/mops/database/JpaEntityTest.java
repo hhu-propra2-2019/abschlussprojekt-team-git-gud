6b7211db9eeb68c907118a68dbf9de2d4a643b24
matlorr <matvey.lorkish@outlook.de>
2020-03-15 11:28:50 +0100

    DELETE test body in createDateiWithTagsTest

--- src/test/java/de/hhu/propra2/material2/mops/database/JpaEntityTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/database/JpaEntityTest.java
@@ -21,30 +21,7 @@ public class JpaEntityTest {
 
     @Test
     public void createDateiWithTagsTest(){
-        Gruppe gruppe = new Gruppe(0, "Test-Gruppe", "Dies ist eine Testgruppe");
-        User mark = new User(1, "Axel", "Amsel", "axeam101");
-        User axel = new User(2, "Mark", "Meyer", "marme102");
 
-        gruppe.getUsers().add(axel);
-        gruppe.getUsers().add(mark);
-
-        mark.getGruppen().add(gruppe);
-        axel.getGruppen().add(gruppe);
-
-
-        LocalDate testDate = LocalDate.now();
-        Datei datei = new Datei(0, "Test-File", "/Test", "Heinz Tester", testDate, testDate, 500, "txt", gruppe);
-        Tag tag = new Tag(1, "Test");
-        Tag tagTwo = new Tag(2, "Wiederholung");
-
-        datei.getTags().add(tag);
-        datei.getTags().add(tagTwo);
-
-        tag.getDateien().add(datei);
-        tagTwo.getDateien().add(datei);
-
-        gruppeRepository.save(gruppe);
-        dateiRepository.save(datei);
     }
 
 }

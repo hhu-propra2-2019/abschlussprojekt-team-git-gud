81f69cf021f42ee800784f0879c423ce30c4f561
matlorr <matvey.lorkish@outlook.de>
2020-03-15 17:52:17 +0100

    FILL User

--- src/main/java/de/hhu/propra2/material2/mops/domain/models/User.java
+++ src/main/java/de/hhu/propra2/material2/mops/domain/models/User.java
@@ -1,4 +1,21 @@
 package de.hhu.propra2.material2.mops.domain.models;
 
+
+import lombok.Value;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Value
 public class User {
+
+    private final long id;
+    private final String vorname;
+    private final String nachname;
+    private final String keycloakname;
+    private final HashMap<Gruppe, Boolean> belegungUndRechte;
+    public List<Gruppe> getAllGruppen() {
+        return new ArrayList<>(belegungUndRechte.keySet());
+    }
 }

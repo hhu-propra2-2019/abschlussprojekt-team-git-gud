4bb4d7690a798f63cfa11a802744b3e65b688053
D4sherInc <phil.klotzsche@gmail.com>
2020-03-16 14:57:06 +0100

    ADD tests for method dateienDerGruppe in ModelService

--- src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/domain/services/ModelServiceTest.java
@@ -1,10 +1,12 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
+import de.hhu.propra2.material2.mops.Database.DTOs.DateiDTO;
 import de.hhu.propra2.material2.mops.Database.DTOs.GruppeDTO;
 import de.hhu.propra2.material2.mops.Database.DTOs.UserDTO;
 import de.hhu.propra2.material2.mops.Database.DateiRepository;
 import de.hhu.propra2.material2.mops.Database.GruppeRepository;
 import de.hhu.propra2.material2.mops.Database.UserRepository;
+import de.hhu.propra2.material2.mops.domain.models.Datei;
 import de.hhu.propra2.material2.mops.domain.models.Gruppe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,13 +14,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("checkstyle:magicnumber")
 @ExtendWith(MockitoExtension.class)
 public class ModelServiceTest {
 
@@ -32,6 +40,12 @@ public class ModelServiceTest {
     private GruppeDTO gruppeDTOMock1;
     @Mock
     private GruppeDTO gruppeDTOMock2;
+    @Mock
+    private DateiDTO dateiDTOMock1;
+    @Mock
+    private DateiDTO dateiDTOMock2;
+    @Mock
+    private DateiDTO dateiDTOMock3;
 
     private UserDTO angelaDTO;
 
@@ -74,4 +88,75 @@ public class ModelServiceTest {
 
         assertThat(result.size(), is(2));
     }
+
+    @Test
+    public void dateienDerGruppeZeroFiles() {
+        HashMap<GruppeDTO, Boolean> belegungUndRechte = new HashMap<>();
+        belegungUndRechte.put(gruppeDTOMock1, true);
+        belegungUndRechte.put(gruppeDTOMock2, false);
+        angelaDTO = new UserDTO(1,
+                "Angela",
+                "Merkel",
+                "AngelaKCName",
+                belegungUndRechte);
+        GruppeDTO gruppeDTO = new GruppeDTO(1L, " drei-Datei-Gruppe", new LinkedList<>(), new LinkedList<>());
+
+        List<Datei> result = modelService.dateienDerGruppe(gruppeDTO);
+
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void dateienDerGruppeOneFile() {
+        HashMap<GruppeDTO, Boolean> belegungUndRechte = new HashMap<>();
+        belegungUndRechte.put(gruppeDTOMock1, true);
+        belegungUndRechte.put(gruppeDTOMock2, false);
+        angelaDTO = new UserDTO(1,
+                "Angela",
+                "Merkel",
+                "AngelaKCName",
+                belegungUndRechte);
+
+        when(dateiDTOMock1.getUploader()).thenReturn(angelaDTO);
+        when(dateiDTOMock1.getUploaddatum()).thenReturn(new Date());
+        when(dateiDTOMock1.getVeroeffentlichungsdatum()).thenReturn(new Date());
+
+        List<DateiDTO> dateien = Collections.singletonList(dateiDTOMock1);
+        GruppeDTO gruppeDTO = new GruppeDTO(1L, "eine Datei-Gruppe", new LinkedList<>(), dateien);
+
+        List<Datei> result = modelService.dateienDerGruppe(gruppeDTO);
+
+        assertThat(result.size(), is(1));
+    }
+
+    @Test
+    public void dateienDerGruppeMultipleFiles() {
+        HashMap<GruppeDTO, Boolean> belegungUndRechte = new HashMap<>();
+        belegungUndRechte.put(gruppeDTOMock1, true);
+        belegungUndRechte.put(gruppeDTOMock2, false);
+        angelaDTO = new UserDTO(1,
+                "Angela",
+                "Merkel",
+                "AngelaKCName",
+                belegungUndRechte);
+
+        when(dateiDTOMock1.getUploader()).thenReturn(angelaDTO);
+        when(dateiDTOMock2.getUploader()).thenReturn(angelaDTO);
+        when(dateiDTOMock3.getUploader()).thenReturn(angelaDTO);
+        when(dateiDTOMock1.getUploaddatum()).thenReturn(new Date());
+        when(dateiDTOMock2.getUploaddatum()).thenReturn(new Date());
+        when(dateiDTOMock3.getUploaddatum()).thenReturn(new Date());
+        when(dateiDTOMock1.getVeroeffentlichungsdatum()).thenReturn(new Date());
+        when(dateiDTOMock2.getVeroeffentlichungsdatum()).thenReturn(new Date());
+        when(dateiDTOMock3.getVeroeffentlichungsdatum()).thenReturn(new Date());
+
+        List<DateiDTO> dateien = Arrays.asList(dateiDTOMock1, dateiDTOMock2, dateiDTOMock3);
+        GruppeDTO gruppeDTO = new GruppeDTO(1L, " drei-Datei-Gruppe", new LinkedList<>(), dateien);
+
+        List<Datei> result = modelService.dateienDerGruppe(gruppeDTO);
+
+        assertThat(result.size(), is(3));
+    }
+
+
 }

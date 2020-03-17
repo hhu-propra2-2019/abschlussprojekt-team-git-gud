8252bc0ed956499247328393e6c33d23cb9cd435
yabiy100 <yasin.biyikli@hhu.de>
2020-03-17 15:40:26 +0100

    - CHANGE Account Method to new one

--- src/main/java/de/hhu/propra2/material2/mops/controller/MaterialController.java
+++ src/main/java/de/hhu/propra2/material2/mops/controller/MaterialController.java
@@ -1,13 +1,9 @@
 package de.hhu.propra2.material2.mops.controller;
 
-import de.hhu.propra2.material2.mops.domain.models.Gruppe;
 import de.hhu.propra2.material2.mops.domain.models.Suche;
 import de.hhu.propra2.material2.mops.domain.models.UploadForm;
-import de.hhu.propra2.material2.mops.domain.models.User;
 import de.hhu.propra2.material2.mops.domain.services.IModelService;
-import de.hhu.propra2.material2.mops.security.Account;
-import io.micrometer.core.instrument.MeterRegistry;
-import org.keycloak.KeycloakPrincipal;
+import de.hhu.propra2.material2.mops.domain.services.ModelService;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,8 +15,6 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Set;
 
 /**
  * After the @RolesAllowed Annotation the Syle Code wants a space after
@@ -34,68 +28,27 @@ public class MaterialController {
     @Autowired
     private RestTemplate serviceAccountRestTemplate;
 
-    /**
-     * Beispiel aus der KeycloakDemo
-     * //
-     * THIS IS JUST AN EXAMPLE! DO NOT QUERY A SERVICE IN THE REQUEST/RESPONSE CYCLE!
-     * <p>
-     * var res = Arrays.asList(serviceAccountRestTemplate
-     * .getForEntity("http://localhost:8080/api/text", Entry[].class).getBody());
-     */
-
-    private List<Gruppe> gruppen;
-    private Set<String> tags;
-    private Set<String> dateiTypen;
-    private Set<String> uploader;
-    private Suche suche;
-
-    public MaterialController(final MeterRegistry registry, final IModelService ms) {
-        User user = ms.getUserByKeyCloakName("studentin1");
-        gruppen = user.getAllGruppen();
-        tags = ms.getAlleTagsByUser(user);
-        dateiTypen = ms.getAlleDateiTypenByUser(user);
-        uploader = ms.getAlleUploaderByUser(user);
-    }
-
-    /**
-     * Nimmt das Authentifizierungstoken von Keycloak und erzeugt ein AccountDTO
-     * für die Views.
-     *
-     * @param token keycloak token injection
-     * @return neuen Account der im Template verwendet wird
-     */
-    private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
-        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
-        return new Account(
-                principal.getName(),
-                principal.getKeycloakSecurityContext().getIdToken().getEmail(),
-                null,
-                token.getAccount().getRoles());
-    }
-
-    /**
-     * start routing.
-     *
+    @Autowired
+    private IModelService modelService;
+    /**start routing.
      * @return String
      */
     @GetMapping("/")
     public String startseite(final KeycloakAuthenticationToken token, final Model model) {
         if (token != null) {
-            model.addAttribute("account", createAccountFromPrincipal(token));
+            model.addAttribute("account", modelService.getAccountFromKeycloak(token));
         }
-        model.addAttribute("gruppen", gruppen);
+        model.addAttribute("gruppen", modelService.getAlleGruppenByUser(token));
         return "start";
     }
 
-    /**
-     * Shows the documents of a Group.
-     *
+    /**Shows the documents of a Group.
      * @return String
      */
     @GetMapping("/dateiSicht")
     @RolesAllowed( {"ROLE_orga", "ROLE_studentin"})
-    public String sicht(final KeycloakAuthenticationToken token, final Model model) {
-        model.addAttribute("account", createAccountFromPrincipal(token));
+    public String sicht(final KeycloakAuthenticationToken token, final Model model, final Long gruppenId) {
+        model.addAttribute("account", modelService.getAccountFromKeycloak(token));
         model.addAttribute("gruppen", gruppen);
         return "dateiSicht";
     }
@@ -109,7 +62,7 @@ public class MaterialController {
     @GetMapping("/suche")
     @RolesAllowed( {"ROLE_orga", "ROLE_studentin"})
     public String vorSuche(final KeycloakAuthenticationToken token, final Model model) {
-        model.addAttribute("account", createAccountFromPrincipal(token));
+        model.addAttribute("account", modelService.getAccountFromKeycloak(token));;
         model.addAttribute("gruppen", gruppen);
         model.addAttribute("tags", tags);
         model.addAttribute("dateiTypen", dateiTypen);
@@ -128,13 +81,13 @@ public class MaterialController {
     public String suchen(
             final KeycloakAuthenticationToken token, final Model model, final @ModelAttribute Suche suchen,
             final String search) {
-        model.addAttribute("account", createAccountFromPrincipal(token));
+        model.addAttribute("account", modelService.getAccountFromKeycloak(token));
         model.addAttribute("gruppen", gruppen);
         model.addAttribute("tags", tags);
         model.addAttribute("dateiTypen", dateiTypen);
         model.addAttribute("uploader", uploader);
-        this.suche = suchen;
         if (search == null) {
+            model.addAttribute("suche", suchen);
             return "redirect:/suche";
         }
         return "redirect:/";
@@ -148,7 +101,7 @@ public class MaterialController {
     @GetMapping("/upload")
     @RolesAllowed( {"ROLE_orga", "ROLE_studentin"})
     public String upload(final KeycloakAuthenticationToken token, final Model model) {
-        model.addAttribute("account", createAccountFromPrincipal(token));
+        model.addAttribute("account", modelService.getAccountFromKeycloak(token));
         model.addAttribute("gruppen", gruppen);
         model.addAttribute("tagText", tags);
         model.addAttribute("uploader", uploader);
@@ -166,7 +119,7 @@ public class MaterialController {
     @PostMapping("/upload")
     @RolesAllowed( {"ROLE_orga", "ROLE_studentin"})
     public String upload(final KeycloakAuthenticationToken token, final Model model, final UploadForm upForm) {
-        model.addAttribute("account", createAccountFromPrincipal(token));
+        model.addAttribute("account", modelService.getAccountFromKeycloak(token));
         System.out.println(upForm);
         return "redirect:/upload";
     }

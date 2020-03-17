32276bce575ff37797b685200165104319aa2a64
yabiy100 <yasin.biyikli@hhu.de>
2020-03-17 14:32:34 +0100

    - ADD suche and getSuchergebnisse

--- src/main/java/de/hhu/propra2/material2/mops/domain/services/IModelService.java
+++ src/main/java/de/hhu/propra2/material2/mops/domain/services/IModelService.java
@@ -1,23 +1,26 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
+import ch.qos.logback.core.subst.Token;
 import de.hhu.propra2.material2.mops.domain.models.Datei;
 import de.hhu.propra2.material2.mops.domain.models.Gruppe;
+import de.hhu.propra2.material2.mops.domain.models.Suche;
 import de.hhu.propra2.material2.mops.domain.models.User;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 
 import java.util.List;
 import java.util.Set;
 
 public interface IModelService {
 
-    List<Gruppe> getAlleGruppenByUser(User user);
+    List<Gruppe> getAlleGruppenByUser(KeycloakAuthenticationToken token);
 
     List<Datei> getAlleDateienByGruppe(Gruppe gruppe);
 
-    Set<String> getAlleTagsByUser(User user);
+    Set<String> getAlleTagsByUser(KeycloakAuthenticationToken token);
 
     Set<String> getAlleTagsByGruppe(Gruppe gruppe);
 
-    Set<String> getAlleDateiTypenByUser(User user);
+    Set<String> getAlleDateiTypenByUser(KeycloakAuthenticationToken token);
 
     Set<String> getAlleDateiTypenByGruppe(Gruppe gruppe);
 
@@ -28,5 +31,9 @@ public interface IModelService {
     User getUserByKeyCloakName(String name);
 
     Set<String> getAlleKategorienByFiles(List<Datei> dateien);
+
+    void suchen(Suche suche);
+
+    List<Datei> getSuchergebnisse(Token token);
 
 }

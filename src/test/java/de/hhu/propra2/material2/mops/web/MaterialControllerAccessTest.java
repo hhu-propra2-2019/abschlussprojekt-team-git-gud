01f72db581a090fc52e912de76989a418e8f0b62
Jakhes <dean.schmitz@schmitzbauer.de>
2020-03-19 14:36:17 +0100

    ADD access tests for /files

--- src/test/java/de/hhu/propra2/material2/mops/web/MaterialControllerAccessTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/web/MaterialControllerAccessTest.java
@@ -1,6 +1,7 @@
 package de.hhu.propra2.material2.mops.web;
 
 import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import de.hhu.propra2.material2.mops.domain.services.MinioDownloadService;
 import de.hhu.propra2.material2.mops.domain.services.ModelService;
 import org.junit.jupiter.api.Test;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
@@ -10,6 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,7 +33,48 @@ public class MaterialControllerAccessTest {
     @MockBean
     ModelService modelService;
 
-    //Public User Access tests
+    @MockBean
+    MinioDownloadService minioDownloadService;
+
+    //Unknown User Access tests
+
+    @Test
+    void testStart_UnknownUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testSuche_UnknownUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().is3xxRedirection());
+
+        mvc.perform(post("/suche").with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testUpload_UnknownUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().is3xxRedirection());
+
+        mvc.perform(post("/upload").with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testDateisicht_UnknownUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testFileDownload_UnknownUser() throws Exception {
+        mvc.perform(get("/files"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    //Not Allowed Role Access tests
 
     @Test
     @WithMockKeycloackAuth(name = "BennyGoodman", roles = "TESTER")
@@ -59,6 +107,13 @@ public class MaterialControllerAccessTest {
     @WithMockKeycloackAuth(name = "Benny Goodman", roles = "TESTER")
     void testDateisicht_PublicUser() throws Exception {
         mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Benny Goodman", roles = "TESTER")
+    void testFileDownload_PublicUser() throws Exception {
+        mvc.perform(get("/files"))
                 .andExpect(status().isForbidden());
     }
 
@@ -101,6 +156,17 @@ public class MaterialControllerAccessTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @WithMockKeycloackAuth(name = "Bruce W.", roles = "studentin")
+    void testFileDownload_StudentUser() throws Exception {
+        String myString = "hello";
+        InputStream inputStream = new ByteArrayInputStream( myString.getBytes() );
+        when(minioDownloadService.getObject(any())).thenReturn(inputStream);
+
+        mvc.perform(get("/files"))
+                .andExpect(status().isOk());
+    }
+
     //Orga User Access tests
 
     @Test
@@ -139,6 +205,17 @@ public class MaterialControllerAccessTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @WithMockKeycloackAuth(name = "Donald T.", roles = "orga")
+    void testFileDownload_OrgaUser() throws Exception {
+        String myString = "hello";
+        InputStream inputStream = new ByteArrayInputStream( myString.getBytes() );
+        when(minioDownloadService.getObject(any())).thenReturn(inputStream);
+
+        mvc.perform(get("/files"))
+                .andExpect(status().isOk());
+    }
+
     //Actuator User Access tests
 
     @Test
@@ -174,6 +251,17 @@ public class MaterialControllerAccessTest {
     @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
     void testDateisicht_ActuatorUser() throws Exception {
         mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
+    void testFileDownload_ActuatorUser() throws Exception {
+        String myString = "hello";
+        InputStream inputStream = new ByteArrayInputStream( myString.getBytes() );
+        when(minioDownloadService.getObject(any())).thenReturn(inputStream);
+
+        mvc.perform(get("/files"))
                 .andExpect(status().isOk());
     }
 }

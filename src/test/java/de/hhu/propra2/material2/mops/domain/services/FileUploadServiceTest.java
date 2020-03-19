b70e7d6a313520c2fae096a305b888b7dc713c1f
magoo100 <magoo100@uni-duesseldorf.de>
2020-03-19 13:28:27 +0100

    add test with minio testcontainer

--- src/test/java/de/hhu/propra2/material2/mops/domain/services/FileUploadServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/domain/services/FileUploadServiceTest.java
@@ -1,60 +1,69 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidBucketNameException;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
-import io.minio.errors.InvalidResponseException;
-import io.minio.errors.NoResponseException;
-import io.minio.errors.RegionConflictException;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.xmlpull.v1.XmlPullParserException;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class FileUploadServiceTest {
+    private static final String ACCESS_KEY = "admin";
+    private static final String SECRET_KEY = "12345678";
+    private static final String TEST_BUCKET_NAME = "testbucket";
+    private static final int MINIO_PORT = 9000;
+    private static final int STARTUP_TIMEOUT = 10;
 
-    @Mock
-    private MultipartFile fileMock;
+    private static GenericContainer minioServer;
+    private static String minioServerUrl;
+    private static FileUploadService fileUploadService;
 
-    private FileUploadService fileUploadService;
+    @BeforeAll
+    static void setUp() throws Exception {
+        minioServer = new GenericContainer("minio/minio")
+                .withEnv("MINIO_ACCESS_KEY", ACCESS_KEY)
+                .withEnv("MINIO_SECRET_KEY", SECRET_KEY)
+                .withCommand("server /data")
+                .withExposedPorts(MINIO_PORT)
+                .waitingFor(new HttpWaitStrategy()
+                        .forPath("/minio/health/ready")
+                        .forPort(MINIO_PORT)
+                        .withStartupTimeout(Duration.ofSeconds(STARTUP_TIMEOUT)));
+        minioServer.start();
 
-    @BeforeEach
-    public void setUp() throws InvalidPortException,
-            InvalidEndpointException,
-            IOException, InvalidKeyException,
-            NoSuchAlgorithmException, InsufficientDataException,
-            InvalidResponseException, InternalException,
-            NoResponseException, InvalidBucketNameException,
-            XmlPullParserException, ErrorResponseException,
-            RegionConflictException {
+        Integer mappedPort = minioServer.getFirstMappedPort();
+        Testcontainers.exposeHostPorts(mappedPort);
+        minioServerUrl = String.format("http://%s:%s", minioServer.getContainerIpAddress(), mappedPort);
 
         MinIOProperties minIOProperties = new MinIOProperties();
-        minIOProperties.setEndpoint("http://localhost:23307/");
-        minIOProperties.setAccesskey("minio");
-        minIOProperties.setSecretkey("minio123");
-        minIOProperties.setBucketname("materialsammlungtest");
+        minIOProperties.setEndpoint(minioServerUrl);
+        minIOProperties.setAccesskey(ACCESS_KEY);
+        minIOProperties.setSecretkey(SECRET_KEY);
+        minIOProperties.setBucketname(TEST_BUCKET_NAME);
 
-        this.fileUploadService = new FileUploadService(minIOProperties);
+        fileUploadService = new FileUploadService(minIOProperties);
     }
 
     @Test
-    public void testUploadNameMaking1() {
-        String fileName = "newFileName";
+    public void uploadFile() {
+        String dateiIdAsString = "123";
+        MultipartFile file = new MockMultipartFile("test.txt",
+                "test.txt",
+                "text/plain",
+                ("The first rule of Fight Club is: You do not talk about Fight Club."
+                        + "The second rule of Fight Club is: You do not talk about Fight Club.")
+                        .getBytes(StandardCharsets.UTF_8));
 
-        boolean result = fileUploadService.upload(fileMock, fileName);
+        boolean result = fileUploadService.upload(file, dateiIdAsString);
 
         assertTrue(result);
     }

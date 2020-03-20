c315e64d3d8e0e9f92da5567a41de5f5c8de29b5
Jakhes <dean.schmitz@schmitzbauer.de>
2020-03-20 16:45:27 +0100

    ADD Tests

---
+++ src/test/java/de/hhu/propra2/material2/mops/web/MaterialControllerAccessTest.java
@@ -0,0 +1,205 @@
+package de.hhu.propra2.material2.mops.web;
+
+import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import de.hhu.propra2.material2.mops.domain.services.MinioDownloadService;
+import de.hhu.propra2.material2.mops.domain.services.ModelService;
+import de.hhu.propra2.material2.mops.domain.services.UploadService;
+import org.junit.jupiter.api.Test;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ComponentScan(basePackageClasses = { KeycloakSecurityComponents.class, KeycloakSpringBootConfigResolver.class })
+public class MaterialControllerAccessTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ModelService modelService;
+
+    @MockBean
+    private MinioDownloadService minioDownloadService;
+
+    @MockBean
+    private UploadService uploadService;
+
+    //Unknown User Access tests
+
+    @Test
+    void testStartUnknownUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testSucheUnknownUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().is3xxRedirection());
+
+        mvc.perform(post("/suche").with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testUploadUnknownUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().is3xxRedirection());
+
+    }
+
+    @Test
+    void testDateisichtUnknownUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    //Not Allowed Role Access tests
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "TESTER")
+    void testStartPublicUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Benny Goodman", roles = "TESTER")
+    void testSuchePublicUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().isForbidden());
+
+        mvc.perform(post("/suche").with(csrf()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Benny Goodman", roles = "TESTER")
+    void testUploadPublicUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Benny Goodman", roles = "TESTER")
+    void testDateisichtPublicUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isForbidden());
+    }
+
+    //Student User Access tests
+
+    @Test
+    @WithMockKeycloackAuth(name = "Bruce W.", roles = "studentin")
+    void testStartStudentUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Bruce W.", roles = "studentin")
+    void testSucheStudentUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().isOk());
+
+        mvc.perform(post("/suche")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Bruce W.", roles = "studentin")
+    void testUploadStudentUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    @WithMockKeycloackAuth(name = "Bruce W.", roles = "studentin")
+    void testDateisichtStudentUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isOk());
+    }
+
+    //Orga User Access tests
+
+    @Test
+    @WithMockKeycloackAuth(name = "Donald T.", roles = "orga")
+    void testStartOrgaUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Donald T.", roles = "orga")
+    void testSucheOrgaUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().isOk());
+
+        mvc.perform(post("/suche")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Donald T.", roles = "orga")
+    void testUploadOrgaUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Donald T.", roles = "orga")
+    void testDateisichtOrgaUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isOk());
+    }
+
+    //Actuator User Access tests
+
+    @Test
+    @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
+    void testStartActuatorUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
+    void testSucheActuatorUserUser() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(status().isOk());
+
+        mvc.perform(post("/suche")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
+    void testUploadActuatorUser() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "James B.", roles = "actuator")
+    void testDateisichtActuatorUser() throws Exception {
+        mvc.perform(get("/dateiSicht"))
+                .andExpect(status().isOk());
+    }
+
+}
---
+++ src/test/java/de/hhu/propra2/material2/mops/web/MaterialControllerModelTest.java
@@ -0,0 +1,173 @@
+package de.hhu.propra2.material2.mops.web;
+
+import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
+import de.hhu.propra2.material2.mops.domain.models.Gruppe;
+import de.hhu.propra2.material2.mops.domain.services.MinioDownloadService;
+import de.hhu.propra2.material2.mops.domain.services.ModelService;
+import de.hhu.propra2.material2.mops.domain.services.UploadService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
+@ComponentScan(basePackageClasses = { KeycloakSecurityComponents.class, KeycloakSpringBootConfigResolver.class })
+public class MaterialControllerModelTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ModelService modelService;
+
+    @MockBean
+    private MinioDownloadService minioDownloadService;
+
+    @MockBean
+    private UploadService uploadService;
+
+    /**init for the tests.
+     *
+     */
+    @BeforeEach
+    void init() {
+        List<Gruppe> gruppen = new ArrayList<>();
+        gruppen.add(new Gruppe(1, "ProPra", null));
+        gruppen.add(new Gruppe(2, "RDB", null));
+        Set<String> tags = new HashSet<>();
+        tags.add("Vorlesung");
+        tags.add("Übung");
+        Set<String> uploader = new HashSet<>();
+        uploader.add("Chris");
+        uploader.add("Christian");
+        uploader.add("Christiano Ronaldo");
+        Set<String> dateiTypen = new HashSet<>();
+        dateiTypen.add("XML");
+        dateiTypen.add("JSON");
+        when(modelService.getAlleGruppenByUser(any())).thenReturn(gruppen);
+        when(modelService.getAlleTagsByUser(any())).thenReturn(tags);
+        when(modelService.getAlleUploaderByUser(any())).thenReturn(uploader);
+        when(modelService.getAlleDateiTypenByUser(any())).thenReturn(dateiTypen);
+    }
+
+    // Startseite Test
+
+    @Test
+    void startEmptyGroupTabsIfUnknownUser() throws Exception {
+        mvc.perform(get("/"));
+        verify(modelService, never()).getAlleGruppenByUser(any());
+
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "TESTER")
+    void startTestGruppenTabsGetCreated() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(content().string(containsString("ProPra")))
+                .andExpect(content().string(containsString("RDB")));
+        verify(modelService, times(1)).getAlleGruppenByUser(any());
+    }
+
+    @Test
+    void testReturnStartUnknownUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(content().string(containsString("Wilkommen bei der Materialsammlung")));
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "TESTER")
+    void testReturnStartLogedInUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(content().string(containsString("Wilkommen bei der Materialsammlung")));
+    }
+
+    // Upload Test
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "studentin")
+    void uploadTestTabsGetCreated() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(content().string(containsString("ProPra")))
+                .andExpect(content().string(containsString("RDB")));
+        verify(modelService, times(1)).getAlleGruppenByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "studentin")
+    void uploadTestTagsGetLoaded() throws Exception {
+        mvc.perform(get("/upload"));
+        verify(modelService, times(1)).getAlleTagsByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "BennyGoodman", roles = "studentin")
+    void testReturnUploadTemplate() throws Exception {
+        mvc.perform(get("/upload"))
+                .andExpect(content().string(containsString("Upload")));
+    }
+
+    //Suche Test
+
+    @Test
+    @WithMockKeycloackAuth(name = "studentin1", roles = "studentin")
+    void sucheTestGruppenTabsGetCreated() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(content().string(containsString("ProPra")))
+                .andExpect(content().string(containsString("RDB")));
+        verify(modelService, times(1)).getAlleGruppenByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "studentin3", roles = "studentin")
+    void sucheTestTagsGetLoaded() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(content().string(containsString("Vorlesung")))
+                .andExpect(content().string(containsString("Übung")));
+        verify(modelService, times(1)).getAlleTagsByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "studentin2", roles = "studentin")
+    void sucheTestDateiTypenGetLoaded() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(content().string(containsString("JSON")))
+                .andExpect(content().string(containsString("XML")));
+        verify(modelService, times(1)).getAlleUploaderByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "studentin1", roles = "studentin")
+    void sucheTestUploaderGetLoaded() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(content().string(containsString("Chris")))
+                .andExpect(content().string(containsString("Christian")))
+                .andExpect(content().string(containsString("Christiano Ronaldo")));
+        verify(modelService, times(1)).getAlleUploaderByUser(any());
+    }
+
+    @Test
+    @WithMockKeycloackAuth(name = "Max Mustermann", roles = "studentin")
+    void testReturnSucheTemplate() throws Exception {
+        mvc.perform(get("/suche"))
+                .andExpect(content().string(containsString("Suche")));
+    }
+}

da6c641c662fa4afde7938c24cbfcaa878547d98
Koybu <sasch223@hhu.de>
2020-03-23 16:11:32 +0100

    ADD startUpdate to UpdateService

--- src/main/java/de/hhu/propra2/material2/mops/domain/services/IUpdateService.java
+++ src/main/java/de/hhu/propra2/material2/mops/domain/services/IUpdateService.java
@@ -1,9 +1,11 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
+import de.hhu.propra2.material2.mops.Exceptions.NoUploadPermissionException;
 import de.hhu.propra2.material2.mops.domain.models.UpdateForm;
 
 import java.sql.SQLException;
 
 public interface IUpdateService {
-    void startUpdate(UpdateForm upForm, String user, Long gruppenId, Long dateiId) throws SQLException;
+    void startUpdate(UpdateForm upForm, String user, Long gruppenId, Long dateiId)
+            throws SQLException, NoUploadPermissionException;
 }
--- src/main/java/de/hhu/propra2/material2/mops/domain/services/UpdateService.java
+++ src/main/java/de/hhu/propra2/material2/mops/domain/services/UpdateService.java
@@ -1,21 +1,33 @@
 package de.hhu.propra2.material2.mops.domain.services;
 
+import com.google.common.base.Strings;
+import de.hhu.propra2.material2.mops.Database.DTOs.UserDTO;
+import de.hhu.propra2.material2.mops.Database.Repository;
+import de.hhu.propra2.material2.mops.Exceptions.NoUploadPermissionException;
 import de.hhu.propra2.material2.mops.domain.models.Datei;
+import de.hhu.propra2.material2.mops.domain.models.Gruppe;
 import de.hhu.propra2.material2.mops.domain.models.Tag;
+import de.hhu.propra2.material2.mops.domain.models.UpdateForm;
+import de.hhu.propra2.material2.mops.domain.models.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class UpdateService {
+public class UpdateService implements IUpdateService {
 
+    private final Repository repository;
     private final ModelService modelService;
 
-    public UpdateService(final ModelService modelServiceArg) {
+    public UpdateService(final Repository repositoryArg,
+                         final ModelService modelServiceArg) {
+        this.repository = repositoryArg;
         this.modelService = modelServiceArg;
     }
 
@@ -27,7 +39,7 @@ public class UpdateService {
      * @return A Datei object which represents the saved File
      */
     @Transactional
-    public Datei dateiUpdate(final Long dateiId, final Long gruppenId, final LocalDate veroeffentlichungsdatum,
+    private Datei dateiUpdate(final Long dateiId, final Long gruppenId, final LocalDate veroeffentlichungsdatum,
                              final List<Tag> tags) throws SQLException {
         Datei datei = modelService.findDateiById(dateiId);
         Datei changedDatei = new Datei(dateiId, datei.getName(), datei.getUploader(),
@@ -38,5 +50,55 @@ public class UpdateService {
         modelService.saveDatei(changedDatei, gruppenId);
 
         return changedDatei;
+    }
+
+    /**
+     * for Update of file.
+     * @param upForm Update Form
+     * @param keycloakUserName KeycloakName of the User
+     * @param gruppenId Id of the group where the file is saved
+     * @param dateiId The id of the file
+     * @throws SQLException
+     * @throws NoUploadPermissionException
+     */
+    @Override
+    public void startUpdate(final UpdateForm upForm,
+                            final String keycloakUserName,
+                            final Long gruppenId,
+                            final Long dateiId)
+            throws SQLException, NoUploadPermissionException {
+
+        UserDTO userDTO = repository.findUserByKeycloakname(keycloakUserName);
+        User user = modelService.loadUser(userDTO);
+
+        Gruppe gruppe = user.getGruppeById(gruppenId);
+
+        if (!user.hasUploadPermission(gruppe)) {
+            throw new NoUploadPermissionException("User has no upload permission");
+        }
+
+        dateiUpdate(dateiId,
+                gruppenId,
+                parseStringToDate(upForm.getTimedUpload()),
+                convertSeperatedStringToList(upForm.getSelectedTags()));
+    }
+
+    private ArrayList<Tag> convertSeperatedStringToList(final String tagStrings) {
+        if (Strings.isNullOrEmpty(tagStrings.trim())) {
+            return new ArrayList<>();
+        }
+        ArrayList<String> tagTexts = Arrays.stream(tagStrings.split(","))
+                .map(String::trim)
+                .map(String::toLowerCase).collect(Collectors.toCollection(ArrayList::new));
+
+        ArrayList<Tag> tags = new ArrayList<>();
+        for (String tagText : tagTexts) {
+            tags.add(new Tag(1, tagText));
+        }
+        return tags;
+    }
+
+    private LocalDate parseStringToDate(final String dateString) {
+        return Strings.isNullOrEmpty(dateString) ? null : LocalDate.parse(dateString);
     }
 }

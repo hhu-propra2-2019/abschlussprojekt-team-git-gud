e5a49ca1da8fe8f3fdcb4177f452f5a16a10d33d
matlorr <matvey.lorkish@outlook.de>
2020-03-24 13:31:24 +0100

    WORKING == flase

--- src/main/java/de/hhu/propra2/material2/mops/web/dto/WebDTOService.java
+++ src/main/java/de/hhu/propra2/material2/mops/web/dto/WebDTOService.java
@@ -4,30 +4,15 @@ package de.hhu.propra2.material2.mops.web.dto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hhu.propra2.material2.mops.security.KeycloakConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WebDTOService {
-    /**
-     * @return
-     */
-    public String loadUpdateWrapperKeyCloakConfig() {
-         KeycloakConfig config = new KeycloakConfig();
-         RestTemplate serviceAccountRestTemplate = config.serviceAccountRestTemplate();
-         final String URL = "http://localhost:8080/yolo";
-         String response = serviceAccountRestTemplate.getForEntity(URL, String.class).getBody();
-         return response;
-    }
 
-    public UpdatedGroupRequestMapper loadUpdateWrapper() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        String ressource = "http://localhost:8080/yolo";
-        String response = restTemplate.getForObject(ressource, String.class);
+    public void loadUpdatedGroupRequestMapper(){
 
-        ObjectMapper mapper = new ObjectMapper();
-        UpdatedGroupRequestMapper result = mapper.readValue(response, UpdatedGroupRequestMapper.class);
-        return result;
     }
 }
--- src/test/java/de/hhu/propra2/material2/mops/web/dto/WebDTOServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/web/dto/WebDTOServiceTest.java
@@ -5,62 +5,44 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import static org.hamcrest.Matchers.is;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.sameInstance;
 
 @ExtendWith(MockitoExtension.class)
 public class WebDTOServiceTest {
 
-    private String jsonFile;
-    private WebDTOService webDTOService;
+    private String jsonExample;
+    @Autowired
+    private RestTemplate serviceAccountRestTemplate;
 
     @BeforeEach
-    private void setup() throws JsonProcessingException {
-        UserWebDTO donald = new UserWebDTO("789@yahoo.de", "Trump", "Donald", "0");
-        UserWebDTO matvey = new UserWebDTO("123@yahoo.de", "Lorkish", "Matvey", "1");
-        UserWebDTO torben = new UserWebDTO("456@yahoo.de", "Schmitz", "Torben", "2");
-        UserWebDTO[] user1 = {matvey, torben};
-        UserWebDTO[] user2 = {donald};
-
-        Map<String, String> belegungenUndRechte1 = new HashMap<>();
-        belegungenUndRechte1.put(matvey.getUserId(), "MEMBER");
-        belegungenUndRechte1.put(torben.getUserId(), "MEMBER");
-        Map<String, String> belegungenUndRechte2 = new HashMap<>();
-        belegungenUndRechte2.put(donald.getUserId(), "ADMIN");
-
-        GroupWebDTO gruppe1 = new GroupWebDTO("Toll",
-                1,
-                Arrays.asList(user1),
-                1,
-                belegungenUndRechte1,
-                "TolleGruppe",
-                "SIMPLE",
-                "PUBLIC");
-        GroupWebDTO gruppe2 = new GroupWebDTO("Nice",
-                2,
-                Arrays.asList(user2),
-                2,
-                belegungenUndRechte2,
-                "DoofeGruppe",
-                "LECTURE",
-                "PRIVATE");
-        GroupWebDTO[] gruppenArray = {gruppe1, gruppe2};
-
-        UpdatedGroupRequestMapper sample = new UpdatedGroupRequestMapper(Arrays.asList(gruppenArray), 1);
-        ObjectMapper mapper = new ObjectMapper();
-        jsonFile = mapper.writeValueAsString(sample);
-
-        webDTOService = new WebDTOService();
+    private void setup() throws IOException {
+        String test  = System.getProperty("user.dir");
+        //jsonExample = new String(Files.readAllBytes(Paths.get("example.json")));
+        System.out.println(test);
     }
 
     @Test
     public void methodGeneratesTheRightObject() {
-        UpdatedGroupRequestMapper test = webDTOService.loadUpdatedGroupRequestMapperromGroupManagementAPI();
-        assertThat(test.getGroupList().get(0).getId(), is(1));
+        Mockito.when(serviceAccountRestTemplate.getForEntity("http://localhost:8080/gruppe2//api/updateGroups/0",
+                String.class)).thenReturn(new ResponseEntity(jsonExample, HttpStatus.OK));
     }
 }

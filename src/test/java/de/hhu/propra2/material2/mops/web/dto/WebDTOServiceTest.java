818d0971cc788c09f48a29560975b32c876caf88
matlorr <matvey.lorkish@outlook.de>
2020-03-24 14:15:01 +0100

    SETUP tests

--- src/test/java/de/hhu/propra2/material2/mops/web/dto/WebDTOServiceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/web/dto/WebDTOServiceTest.java
@@ -2,6 +2,7 @@ package de.hhu.propra2.material2.mops.web.dto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hhu.propra2.material2.mops.Material2Application;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -29,20 +31,28 @@ import static org.hamcrest.Matchers.sameInstance;
 @ExtendWith(MockitoExtension.class)
 public class WebDTOServiceTest {
 
-    private String jsonExample;
-    @Autowired
+    @Mock
     private RestTemplate serviceAccountRestTemplate;
+
+    private String jsonExample;
+    private BufferedReader bufferedReader;
 
     @BeforeEach
     private void setup() throws IOException {
-        String test  = System.getProperty("user.dir");
-        //jsonExample = new String(Files.readAllBytes(Paths.get("example.json")));
-        System.out.println(test);
+        jsonExample = "";
+        File file = new File("src/main/resources/example.json");
+        bufferedReader = new BufferedReader(new FileReader(file));
+        bufferedReader.lines().forEach(string -> jsonExample = jsonExample.concat(string));
+        System.out.println(jsonExample.trim());
     }
 
     @Test
     public void methodGeneratesTheRightObject() {
         Mockito.when(serviceAccountRestTemplate.getForEntity("http://localhost:8080/gruppe2//api/updateGroups/0",
-                String.class)).thenReturn(new ResponseEntity(jsonExample, HttpStatus.OK));
+                UpdatedGroupRequestMapper.class)).thenReturn(new ResponseEntity(jsonExample, HttpStatus.OK));
+
+        UpdatedGroupRequestMapper updatedGroupRequestMapper = serviceAccountRestTemplate
+                                .getForEntity("http://localhost:8080/gruppe2//api/updateGroups/0", UpdatedGroupRequestMapper.class)
+                                .getBody();
     }
 }

69452ff89b9db5f2040890a94f853e17e5c95715
Xeadriel <ogulcanasik@web.de>
2020-03-25 16:49:58 +0100

    ADD java docs for public functions and hopefully fix an issue with the performance test

--- src/main/java/de/hhu/propra2/material2/mops/database/Repository.java
+++ src/main/java/de/hhu/propra2/material2/mops/database/Repository.java
@@ -60,7 +60,7 @@ public final class Repository {
      * Lazy (no File Loading) search for a user that loads
      * all his groups, rights but no
      * files assigned to their group with
-     * their tags. Resets group-user relations assigns them again.
+     * their tags.
      * Returns null if no user found.
      *
      * @param keyCloakNameArg
@@ -93,6 +93,8 @@ public final class Repository {
     /**
      * Saves a User with all his groups
      * and rights.
+     * Resets and reassigns all User - Group Relations.
+     *
      * To be used for syncronization
      * with gruppenbildung.
      *
@@ -123,16 +125,17 @@ public final class Repository {
     }
 
     /**
-     * Deletes User by ID.
+     * Deletes User by UserDTO.
      * To be used for synchronization
-     * with gruppenbildung.
+     * with Gruppenbildung. Also removes all its
+     * Group - User relations before deleting the user.
+     * Extra step: changes all its uploaded files uploaderID
+     * to the userID of the placeholder for deletedUsers.
      *
      * @param userDTO
      * @throws SQLException
      */
     public void deleteUserByUserDTO(final UserDTO userDTO) throws SQLException {
-        deleteUserGroupRelationByUserId(userDTO.getId());
-
         PreparedStatement preparedStatement =
                 connection.prepareStatement("delete from User where userID=?");
         preparedStatement.setLong(1, userDTO.getId());
@@ -144,6 +147,14 @@ public final class Repository {
         preparedStatement.close();
     }
 
+    /**
+     * Removes Group - User Relation by UserDTO and GruppeDTO.
+     * Nothing special just another statement call.
+     *
+     * @param userDTO
+     * @param gruppeDTO
+     * @throws SQLException
+     */
     public void deleteUserGroupRelationByUserDTOAndGruppeDTO(final UserDTO userDTO,
                                                              final GruppeDTO gruppeDTO) throws SQLException {
         PreparedStatement preparedStatement =
@@ -180,9 +191,17 @@ public final class Repository {
     }
 
     /**
-     * Saves a file with all its tags
+     * Saves or updates a file with all its tags
      * by dateiDTO and
      * returns its generated ID.
+     *
+     * First it checks if the file already exists in
+     * the database. If so it calls the update function which only
+     * updates the existing files meta data.
+     * If it does not exist it saves the File with its
+     * tags, which also updates tag - datei relations.
+     *
+     * Removes its group from the cache either way.
      *
      * @param dateiDTO
      * @throws SQLException
@@ -238,6 +257,14 @@ public final class Repository {
         }
     }
 
+    /**
+     * Searches for DateiDTO by its ID. Nothing special. Just an SQL query.
+     * Returns null if no file found.
+     *
+     * @param id
+     * @return
+     * @throws SQLException
+     */
     public DateiDTO findDateiById(final long id) throws SQLException {
         DateiDTO datei = null;
 
@@ -266,6 +293,18 @@ public final class Repository {
         return datei;
     }
 
+    /**
+     * Returns a LinkedList of DateiDTO by GruppeDTO.
+     * Checks the cache first for the GruppeDTO and
+     * proceeds with looking for the DateiDTOs if
+     * no valid entry can be found in the cache.
+     *
+     * Returns empty LinkedList if no files found.
+     *
+     * @param gruppeDTO
+     * @return
+     * @throws SQLException
+     */
     @SuppressWarnings("checkstyle:MagicNumber")
     public LinkedList<DateiDTO> findAllDateiByGruppeDTO(final GruppeDTO gruppeDTO) throws SQLException {
         GruppeDTO cachedGruppe = gruppeCache.get(gruppeDTO.getId());
@@ -295,6 +334,14 @@ public final class Repository {
         return dateien;
     }
 
+    /**
+     * Deletes Datei by its DTO.
+     * Removes it's tag relations on the way and
+     * removes its own group from the cache.
+     *
+     * @param dateiDTO
+     * @throws SQLException
+     */
     public void deleteDateiByDateiDTO(final DateiDTO dateiDTO) throws SQLException {
         deleteTagRelationsByDateiId(dateiDTO.getId());
 
--- src/test/java/de/hhu/propra2/material2/mops/database/RepositoryPerformanceTest.java
+++ src/test/java/de/hhu/propra2/material2/mops/database/RepositoryPerformanceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.sql.SQLException;
 import java.time.Duration;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Sql("/data.sql")
 @SpringBootTest(classes = Material2Application.class)
 public final class RepositoryPerformanceTest {
 
